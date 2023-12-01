df283b7b462d62b88533357eed06fc2ded04638e
ygo <ka.ra.si.naporitan@gmail.com>
2023-12-01 09:26:30 +0900

    hander追加

--- internal/app/goheki/utility/database.go
+++ internal/app/goheki/utility/database.go
@@ -1,1 +1,17 @@
 package utility
+
+import (
+	"github.com/jmoiron/sqlx"
+
+	"github.com/maguro-alternative/goheki/pkg/db"
+)
+
+type botHandlerDB struct {
+	DBHandler *db.DBHandler
+}
+
+func NewSqlDB(dbSql *sqlx.DB) *botHandlerDB {
+	return &botHandlerDB{
+		DBHandler: db.NewDBHandler(dbSql),
+	}
+}
