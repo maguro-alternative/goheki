28e5e4e877aaf69e1d612d27489ef1cd75782964
ygo <ka.ra.si.naporitan@gmail.com>
2023-12-23 10:55:50 +0900

    json返す

--- internal/app/goheki/api/entry/create.go
+++ internal/app/goheki/api/entry/create.go
@@ -2,6 +2,7 @@ package entry
 
 import (
 	"github.com/maguro-alternative/goheki/internal/app/goheki/service"
+	"github.com/maguro-alternative/goheki/pkg/db"
 
 	"encoding/json"
 	"net/http"
@@ -29,26 +30,26 @@ func (h *CreateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		return
 	}
-	var entry Entry
+	var entry []Entry
 	query := `
 		INSERT INTO entry (
 			name,
 			image,
 			content,
 			create_at
-		) VALUES (
-			:Name,
-			:Image,
-			:Content,
-			:CreateAt
-		)
+		) VALUES (?)
 	`
 	err := json.NewDecoder(r.Body).Decode(&entry)
 	if err != nil {
 		return
 	}
-	_, err = h.svc.DB.NamedExecContext(r.Context(), query, entry)
+	query, args, err := db.In(query, entry)
 	if err != nil {
 		return
 	}
+	_, err = h.svc.DB.ExecContext(r.Context(), query, args...)
+	if err != nil {
+		return
+	}
+	json.NewEncoder(w).Encode(&entry)
 }
