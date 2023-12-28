5a035ddbdafaa85112f4853b66f8c589cfbe2e72
ygo <ka.ra.si.naporitan@gmail.com>
2023-12-28 21:58:54 +0900

    update

--- internal/app/goheki/api/entry/entry_test.go
+++ internal/app/goheki/api/entry/entry_test.go
@@ -183,6 +183,99 @@ func TestEntryHandler(t *testing.T) {
 				CreatedAt: fixedTime,
 			},
 		}
+		updateEntry := []Entry{
+			{
+				Name:      "テストエントリ3",
+				Image:     "https://example.com/image3.png",
+				Content:   "テスト内容3",
+				CreatedAt: fixedTime,
+			},
+			{
+				Name:      "テストエントリ4",
+				Image:     "https://example.com/image4.png",
+				Content:   "テスト内容4",
+				CreatedAt: fixedTime,
+			},
+		}
+		query := `
+			INSERT INTO entry (
+				name,
+				image,
+				content,
+				created_at
+			) VALUES (
+				:name,
+				:image,
+				:content,
+				:created_at
+			)
+		`
+		for _, entry := range entry {
+			_, err = tx.NamedExecContext(ctx, query, entry)
+			assert.NoError(t, err)
+		}
+		var indexService = service.NewIndexService(
+			tx,
+			cookie.Store,
+			env,
+		)
+		// テストの実行
+		h := NewReadHandler(indexService)
+		eJson, err := json.Marshal(&updateEntry)
+		req, err := http.NewRequest(http.MethodPost, "/api/entry/update", bytes.NewBuffer(eJson))
+		assert.NoError(t, err)
+
+		err = json.NewDecoder(req.Body).Decode(&updateEntry)
+		assert.NoError(t, err)
+
+		w := httptest.NewRecorder()
+
+		assert.NoError(t, err)
+
+		// テストの実行
+		h.ServeHTTP(w, req)
+
+		// ロールバック
+		tx.RollbackCtx(ctx)
+
+		// 応答の検証
+		res := w.Result()
+		assert.Equal(t, http.StatusOK, res.StatusCode)
+
+		var actual []Entry
+		err = json.NewDecoder(res.Body).Decode(&actual)
+		assert.NoError(t, err)
+
+		assert.Equal(t, entry, actual)
+	})
+
+	t.Run("entry削除", func(t *testing.T) {
+		ctx := context.Background()
+		env, err := envconfig.NewEnv()
+		assert.NoError(t, err)
+		// データベースに接続
+		indexDB, cleanup, err := db.NewDBV1(ctx, "postgres", env.DatabaseURL)
+		assert.NoError(t, err)
+		defer cleanup()
+		// トランザクションの開始
+		tx, err := indexDB.BeginTxx(ctx, nil)
+		assert.NoError(t, err)
+		fixedTime := time.Date(2023, time.December, 27, 10, 55, 22, 0, time.UTC)
+		// テストデータの準備
+		entry := []Entry{
+			{
+				Name:      "テストエントリ1",
+				Image:     "https://example.com/image1.png",
+				Content:   "テスト内容1",
+				CreatedAt: fixedTime,
+			},
+			{
+				Name:      "テストエントリ2",
+				Image:     "https://example.com/image2.png",
+				Content:   "テスト内容2",
+				CreatedAt: fixedTime,
+			},
+		}
 		query := `
 			INSERT INTO entry (
 				name,
@@ -208,7 +301,7 @@ func TestEntryHandler(t *testing.T) {
 		// テストの実行
 		h := NewReadHandler(indexService)
 		eJson, err := json.Marshal(&entry)
-		req, err := http.NewRequest(http.MethodPost, "/api/entry/update", bytes.NewBuffer(eJson))
+		req, err := http.NewRequest(http.MethodPost, "/api/entry/delete", bytes.NewBuffer(eJson))
 		assert.NoError(t, err)
 
 		err = json.NewDecoder(req.Body).Decode(&entry)
