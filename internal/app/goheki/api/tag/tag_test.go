79b6f5a6e57b2b5baf64f0d46ff3da2748d5c7a2
ygo <ka.ra.si.naporitan@gmail.com>
2023-12-30 08:49:08 +0900

    createtest

--- internal/app/goheki/api/tag/tag_test.go
+++ internal/app/goheki/api/tag/tag_test.go
@@ -1,1 +1,67 @@
 package tag
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/maguro-alternative/goheki/configs/envconfig"
+	"github.com/maguro-alternative/goheki/internal/app/goheki/service"
+	"github.com/maguro-alternative/goheki/internal/app/goheki/service/cookie"
+
+	"github.com/maguro-alternative/goheki/pkg/db"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTagHandler(t *testing.T) {
+	t.Run("tag登録", func(t *testing.T) {
+		ctx := context.Background()
+		env, err := envconfig.NewEnv()
+		assert.NoError(t, err)
+		// データベースに接続
+		indexDB, cleanup, err := db.NewDBV1(ctx, "postgres", env.DatabaseURL)
+		assert.NoError(t, err)
+		defer cleanup()
+		// トランザクションの開始
+		tx, err := indexDB.BeginTxx(ctx, nil)
+		assert.NoError(t, err)
+		// テストデータの準備
+		tag := []Tag{
+			{
+				Name:      "テストタグ1",
+			},
+			{
+				Name:      "テストタグ2",
+			},
+		}
+
+		var indexService = service.NewIndexService(
+			tx,
+			cookie.Store,
+			env,
+		)
+		// テストの実行
+		h := NewCreateHandler(indexService)
+		eJson, err := json.Marshal(&tag)
+		req, err := http.NewRequest(http.MethodPost, "/api/tag/create", bytes.NewBuffer(eJson))
+		assert.NoError(t, err)
+
+		w := httptest.NewRecorder()
+		h.ServeHTTP(w, req)
+
+		tx.RollbackCtx(ctx)
+
+		assert.Equal(t, http.StatusOK, w.Code)
+
+		var tags []Tag
+		err = json.NewDecoder(w.Body).Decode(&tags)
+		assert.NoError(t, err)
+
+		assert.Equal(t, tag[0].Name, tags[0].Name)
+		assert.Equal(t, tag[1].Name, tags[1].Name)
+	})
+}
