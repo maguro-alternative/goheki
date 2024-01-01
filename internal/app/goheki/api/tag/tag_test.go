619d0343a567d8c5d0590b65aeb1f9e6586f166a
ygo <ka.ra.si.naporitan@gmail.com>
2024-01-01 15:32:09 +0900

    test

--- internal/app/goheki/api/tag/tag_test.go
+++ internal/app/goheki/api/tag/tag_test.go
@@ -32,10 +32,10 @@ func TestTagHandler(t *testing.T) {
 		// テストデータの準備
 		tag := []Tag{
 			{
-				Name:      "テストタグ1",
+				Name: "テストタグ1",
 			},
 			{
-				Name:      "テストタグ2",
+				Name: "テストタグ2",
 			},
 		}
 
@@ -79,10 +79,10 @@ func TestTagHandler(t *testing.T) {
 		// テストデータの準備
 		tag := []Tag{
 			{
-				Name:      "テストタグ1",
+				Name: "テストタグ1",
 			},
 			{
-				Name:      "テストタグ2",
+				Name: "テストタグ2",
 			},
 		}
 
@@ -122,5 +122,71 @@ func TestTagHandler(t *testing.T) {
 
 		assert.Equal(t, tag[0].Name, tags[0].Name)
 		assert.Equal(t, tag[1].Name, tags[1].Name)
+	})
+
+	t.Run("tag削除", func(t *testing.T) {
+		ctx := context.Background()
+		env, err := envconfig.NewEnv()
+		assert.NoError(t, err)
+		// データベースに接続
+		indexDB, cleanup, err := db.NewDBV1(ctx, "postgres", env.DatabaseURL)
+		assert.NoError(t, err)
+		defer cleanup()
+		// トランザクションの開始
+		tx, err := indexDB.BeginTxx(ctx, nil)
+		assert.NoError(t, err)
+		// テストデータの準備
+		tag := []Tag{
+			{
+				Name: "テストタグ1",
+			},
+			{
+				Name: "テストタグ2",
+			},
+		}
+		var ids []int64
+
+		query := `
+			INSERT INTO tag (
+				name
+			) VALUES (
+				:name
+			)
+		`
+		for _, tag := range tag {
+			_, err = tx.NamedExecContext(ctx, query, tag)
+			assert.NoError(t, err)
+		}
+		selectQuery := `
+			SELECT
+				id
+			FROM
+				tag
+		`
+		err = tx.SelectContext(ctx, &ids, selectQuery)
+		assert.NoError(t, err)
+		delIDs := DeleteIDs{IDs: ids}
+
+		var indexService = service.NewIndexService(
+			tx,
+			cookie.Store,
+			env,
+		)
+		// テストの実行
+		h := NewDeleteHandler(indexService)
+		eJson, err := json.Marshal(&delIDs)
+		req, err := http.NewRequest(http.MethodDelete, "/api/tag/delete", bytes.NewBuffer(eJson))
+		assert.NoError(t, err)
+
+		w := httptest.NewRecorder()
+		h.ServeHTTP(w, req)
+
+		tx.RollbackCtx(ctx)
+
+		assert.Equal(t, http.StatusOK, w.Code)
+
+		var actual DeleteIDs
+		err = json.NewDecoder(w.Body).Decode(&actual)
+		assert.NoError(t, err)
 	})
 }
