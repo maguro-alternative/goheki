48fbda7abc9d6a7790baa2f5e174861eba2a44d7
ygo <ka.ra.si.naporitan@gmail.com>
2024-01-07 12:03:02 +0900

    en

--- internal/app/goheki/api/tag/tag.go
+++ internal/app/goheki/api/tag/tag.go
@@ -11,15 +11,6 @@ import (
 	"net/http"
 )
 
-type Tag struct {
-	ID        *int64    `db:"id" json:"id"`
-	Name      string    `db:"name" json:"name"`
-}
-
-type IDs struct {
-	IDs []int64 `json:"ids"`
-}
-
 type CreateHandler struct {
 	svc *service.IndexService
 }
