b926bd25824692bb7c5e082931ed9c973dd9383e
ygo <ka.ra.si.naporitan@gmail.com>
2024-01-07 12:02:44 +0900

    en

---
+++ internal/app/goheki/api/source/entity.go
@@ -0,0 +1,8 @@
+package source
+
+type Source struct {
+	ID      *int64 `db:"id" json:"id"`
+	Name    string `db:"name" json:"name"`
+	Url     string `db:"url" json:"url"`
+	Type    string `db:"type" json:"type"`
+}
