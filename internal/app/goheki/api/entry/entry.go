034be9573d82898d8392bf848e01801543c743fb
ygo <ka.ra.si.naporitan@gmail.com>
2024-01-07 12:02:54 +0900

    en

--- internal/app/goheki/api/entry/entry.go
+++ internal/app/goheki/api/entry/entry.go
@@ -9,25 +9,7 @@ import (
 
 	"encoding/json"
 	"net/http"
-	"time"
 )
-
-type Entry struct {
-	ID        *int64    `db:"id" json:"id"`
-	SourceID  int64     `db:"source_id" json:"source_id"`
-	Name      string    `db:"name" json:"name"`
-	Image     string    `db:"image" json:"image"`
-	Content   string    `db:"content" json:"content"`
-	CreatedAt time.Time `db:"created_at" json:"created_at"`
-}
-
-type ID struct {
-	ID int64 `json:"id"`
-}
-
-type IDs struct {
-	IDs []int64 `json:"ids"`
-}
 
 type CreateHandler struct {
 	svc *service.IndexService
