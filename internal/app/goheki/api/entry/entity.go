6d7f5d293fa4d9c12237ed563dde96680a487230
ygo <ka.ra.si.naporitan@gmail.com>
2024-01-08 15:42:55 +0900

    entry

--- internal/app/goheki/api/entry/entity.go
+++ internal/app/goheki/api/entry/entity.go
@@ -13,6 +13,13 @@ type Entry struct {
 	CreatedAt time.Time `db:"created_at" json:"created_at"`
 }
 
+type Source struct {
+	ID   *int64 `db:"id" json:"id"`
+	Name string `db:"name" json:"name"`
+	Url  string `db:"url" json:"url"`
+	Type string `db:"type" json:"type"`
+}
+
 type ID struct {
 	ID int64 `json:"id"`
 }
