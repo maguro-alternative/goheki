db4bc47c16f1b62ba1f8b770ceac2e44ab0ca28a
ygo <ka.ra.si.naporitan@gmail.com>
2024-01-11 14:13:29 +0900

    l

--- internal/app/goheki/model/fixtures/personality.go
+++ internal/app/goheki/model/fixtures/personality.go
@@ -1,7 +1,47 @@
 package fixtures
 
+import (
+	"context"
+	"testing"
+)
+
 type Personality struct {
 	ID      *int64 `db:"id"`
 	EntryID *int64 `db:"entry_id"`
 	Type    string `db:"type"`
+}
+
+func NewPersonality(ctx context.Context) *ModelConnector {
+	personality := &Personality{
+		Type: "introvert",
+	}
+
+	return &ModelConnector{
+		Model: personality,
+		addToFixture: func(t *testing.T, f *Fixture) {
+			f.Personalities = append(f.Personalities, personality)
+		},
+		connect: func(t *testing.T, f *Fixture, connectingModel interface{}) {
+			switch connectingModel.(type) {
+			case *Entry:
+				entry := connectingModel.(*Entry)
+				personality.EntryID = entry.ID
+			default:
+				t.Fatalf("%T cannot be connected to %T", connectingModel, personality)
+			}
+		},
+		insertTable: func(t *testing.T, f *Fixture) {
+			result, err := f.dbv1.NamedExecContext(ctx, "INSERT INTO personality (entry_id, type) VALUES (:entry_id, :type)", personality)
+			if err != nil {
+				t.Fatalf("insert error: %v", err)
+			}
+			// 連番されるIDを取得する
+			id, err := result.LastInsertId()
+			if err != nil {
+				t.Fatal(err)
+			}
+			// 連番されるIDをセットする
+			personality.ID = &id
+		},
+	}
 }
