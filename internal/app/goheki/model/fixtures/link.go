a6d6cd0ead4f254268f137da36e2d548786d5b1c
ygo <ka.ra.si.naporitan@gmail.com>
2024-01-11 14:13:33 +0900

    l

--- internal/app/goheki/model/fixtures/link.go
+++ internal/app/goheki/model/fixtures/link.go
@@ -1,5 +1,10 @@
 package fixtures
 
+import (
+	"context"
+	"testing"
+)
+
 type Link struct {
 	ID       *int64 `db:"id"`
 	EntryID  *int64 `db:"entry_id"`
@@ -7,4 +12,42 @@ type Link struct {
 	URL      string `db:"url"`
 	Nsfw     bool   `db:"nsfw"`
 	Darkness bool   `db:"darkness"`
+}
+
+func NewLink(ctx context.Context) *ModelConnector {
+	link := &Link{
+		Type:     "blog",
+		URL:      "https://example.com",
+		Nsfw:     false,
+		Darkness: false,
+	}
+
+	return &ModelConnector{
+		Model: link,
+		addToFixture: func(t *testing.T, f *Fixture) {
+			f.Links = append(f.Links, link)
+		},
+		connect: func(t *testing.T, f *Fixture, connectingModel interface{}) {
+			switch connectingModel.(type) {
+			case *Entry:
+				entry := connectingModel.(*Entry)
+				link.EntryID = entry.ID
+			default:
+				t.Fatalf("%T cannot be connected to %T", connectingModel, link)
+			}
+		},
+		insertTable: func(t *testing.T, f *Fixture) {
+			result, err := f.dbv1.NamedExecContext(ctx, "INSERT INTO link (entry_id, type, url, nsfw, darkness) VALUES (:entry_id, :type, :url, :nsfw, :darkness)", link)
+			if err != nil {
+				t.Fatalf("insert error: %v", err)
+			}
+			// 連番されるIDを取得する
+			id, err := result.LastInsertId()
+			if err != nil {
+				t.Fatal(err)
+			}
+			// 連番されるIDをセットする
+			link.ID = &id
+		},
+	}
 }
