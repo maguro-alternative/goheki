ab4e8b22294b0b7538afb0f4f889a5cacad576aa
ygo <ka.ra.si.naporitan@gmail.com>
2024-01-11 14:04:11 +0900

    e

--- internal/app/goheki/model/fixtures/heki_radar_chart.go
+++ internal/app/goheki/model/fixtures/heki_radar_chart.go
@@ -1,7 +1,48 @@
 package fixtures
 
+import (
+	"context"
+	"testing"
+)
+
 type HekiRadarChart struct {
 	EntryID *int64 `db:"entry_id"`
 	AI      int64  `db:"ai"`
 	NU      int64  `db:"nu"`
+}
+
+func NewHekiRadarChart(ctx context.Context) *ModelConnector {
+	hekiRadarChart := &HekiRadarChart{
+		AI: 1,
+		NU: 1,
+	}
+
+	return &ModelConnector{
+		Model: hekiRadarChart,
+		addToFixture: func(t *testing.T, f *Fixture) {
+			f.HekiRadarCharts = append(f.HekiRadarCharts, hekiRadarChart)
+		},
+		connect: func(t *testing.T, f *Fixture, connectingModel interface{}) {
+			switch connectingModel.(type) {
+			case *Entry:
+				entry := connectingModel.(*Entry)
+				hekiRadarChart.EntryID = entry.ID
+			default:
+				t.Fatalf("%T cannot be connected to %T", connectingModel, hekiRadarChart)
+			}
+		},
+		insertTable: func(t *testing.T, f *Fixture) {
+			result, err := f.dbv1.NamedExecContext(ctx, "INSERT INTO heki_radar_chart (entry_id, ai, nu) VALUES (:entry_id, :ai, :nu)", hekiRadarChart)
+			if err != nil {
+				t.Fatalf("insert error: %v", err)
+			}
+			// 連番されるIDを取得する
+			id, err := result.LastInsertId()
+			if err != nil {
+				t.Fatal(err)
+			}
+			// 連番されるIDをセットする
+			hekiRadarChart.EntryID = &id
+		},
+	}
 }
