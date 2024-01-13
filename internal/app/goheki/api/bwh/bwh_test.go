b2dfca900d92e17e0f89a6bde2ca1d631bd4e2d5
ygo <ka.ra.si.naporitan@gmail.com>
2024-01-13 11:26:33 +0900

    create

---
+++ internal/app/goheki/api/bwh/bwh_test.go
@@ -0,0 +1,113 @@
+package bwh
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	//"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/maguro-alternative/goheki/configs/envconfig"
+	"github.com/maguro-alternative/goheki/internal/app/goheki/service"
+	"github.com/maguro-alternative/goheki/internal/app/goheki/service/cookie"
+
+	"github.com/maguro-alternative/goheki/pkg/db"
+
+	"github.com/maguro-alternative/goheki/internal/app/goheki/model/fixtures"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateBEHHandler(t *testing.T) {
+	ctx := context.Background()
+	env, err := envconfig.NewEnv()
+	assert.NoError(t, err)
+	yumiHeight := int64(167)
+	takaneHeight := int64(169)
+	takaneWeight := int64(49)
+	// データベースに接続
+	indexDB, cleanup, err := db.NewDBV1(ctx, "postgres", env.DatabaseURL)
+	assert.NoError(t, err)
+	defer cleanup()
+	// トランザクションの開始
+	tx, err := indexDB.BeginTxx(ctx, nil)
+	assert.NoError(t, err)
+
+	fixedTime := time.Date(2023, time.December, 27, 10, 55, 22, 0, time.UTC)
+	// データベースの準備
+	f := &fixtures.Fixture{DBv1: tx}
+	f.Build(t,
+		fixtures.NewSource(ctx, func(s *fixtures.Source) {
+			s.Name = "閃乱カグラ"
+			s.Url = "https://example.com/image1.png"
+			s.Type = "anime"
+		}).Connect(fixtures.NewEntry(ctx, func(s *fixtures.Entry) {
+			s.Name = "雪泉"
+			s.Image = "https://example.com/image1.png"
+			s.Content = "かわいい"
+			s.CreatedAt = fixedTime
+		})),
+		fixtures.NewSource(ctx, func(s *fixtures.Source) {
+			s.Name = "アイドルマスター"
+			s.Url = "https://example.com/image2.png"
+			s.Type = "game"
+		}).Connect(fixtures.NewEntry(ctx, func(s *fixtures.Entry) {
+			s.Name = "四条貴音"
+			s.Image = "https://example.com/image2.png"
+			s.Content = "お姫ちん"
+			s.CreatedAt = fixedTime
+		})),
+		fixtures.NewTag(ctx, func(s *fixtures.Tag) {
+			s.Name = "テストタグ1"
+		}),
+		fixtures.NewTag(ctx, func(s *fixtures.Tag) {
+			s.Name = "テストタグ2"
+		}),
+	)
+
+	// テストデータの準備
+	bwhs := []BWH{
+		{
+			EntryID: f.Entrys[0].ID,
+			Bust:   92,
+			Waist:  56,
+			Hip:    84,
+			Height: &yumiHeight,
+		},
+		{
+			EntryID: f.Entrys[1].ID,
+			Bust:   90,
+			Waist:  60,
+			Hip:    92,
+			Height: &takaneHeight,
+			Weight: &takaneWeight,
+		},
+	}
+	var indexService = service.NewIndexService(
+		tx,
+		cookie.Store,
+		env,
+	)
+	t.Run("bwh登録", func(t *testing.T) {
+		h := NewCreateHandler(indexService)
+		bJson, err := json.Marshal(bwhs)
+		assert.NoError(t, err)
+		req := httptest.NewRequest(http.MethodPost, "/api/bwh/create", bytes.NewBuffer(bJson))
+		assert.NoError(t, err)
+
+		w := httptest.NewRecorder()
+		h.ServeHTTP(w, req)
+
+		tx.RollbackCtx(ctx)
+		assert.Equal(t, http.StatusOK, w.Code)
+
+		var res []BWH
+		err = json.Unmarshal(w.Body.Bytes(), &res)
+		assert.NoError(t, err)
+		assert.Equal(t, bwhs, res)
+	})
+}
+
