e51cc96d3b750d1fbd27bd3ecad4bc322dff1285
ygo <ka.ra.si.naporitan@gmail.com>
2024-01-14 10:58:29 +0900

    update

--- internal/app/goheki/api/hairlength/hairlength_test.go
+++ internal/app/goheki/api/hairlength/hairlength_test.go
@@ -200,3 +200,80 @@ func TestReadHairLengthHandler(t *testing.T) {
 		assert.Equal(t, heirLengths, res)
 	})
 }
+
+func TestUpdateHairLengthHandler(t *testing.T) {
+	ctx := context.Background()
+	env, err := envconfig.NewEnv()
+	assert.NoError(t, err)
+	// データベースに接続
+	indexDB, cleanup, err := db.NewDBV1(ctx, "postgres", env.DatabaseURL)
+	assert.NoError(t, err)
+	defer cleanup()
+	// トランザクションの開始
+	tx, err := indexDB.BeginTxx(ctx, nil)
+	assert.NoError(t, err)
+
+	fixedTime := time.Date(2023, time.December, 27, 10, 55, 22, 0, time.UTC)
+	// データベースの準備
+	f := &fixtures.Fixture{DBv1: tx}
+	f.Build(t,
+		fixtures.NewSource(ctx, func(s *fixtures.Source) {
+			s.Name = "閃乱カグラ"
+			s.Url = "https://example.com/image1.png"
+			s.Type = "anime"
+		}).Connect(fixtures.NewEntry(ctx, func(s *fixtures.Entry) {
+			s.Name = "雪泉"
+			s.Image = "https://example.com/image1.png"
+			s.Content = "かわいい"
+			s.CreatedAt = fixedTime
+		}).Connect(fixtures.NewHairLength(ctx, func(s *fixtures.HairLength) {
+			s.Length = "short"
+		}))),
+		fixtures.NewSource(ctx, func(s *fixtures.Source) {
+			s.Name = "アイドルマスター"
+			s.Url = "https://example.com/image2.png"
+			s.Type = "game"
+		}).Connect(fixtures.NewEntry(ctx, func(s *fixtures.Entry) {
+			s.Name = "四条貴音"
+			s.Image = "https://example.com/image2.png"
+			s.Content = "お姫ちん"
+			s.CreatedAt = fixedTime
+		}).Connect(fixtures.NewHairLength(ctx, func(s *fixtures.HairLength) {
+			s.Length = "long"
+		}))),
+	)
+
+	// テストデータの準備
+	updateHeirLengths := []HairLength{
+		{
+			EntryID: f.Entrys[0].ID,
+			Length:  "long",
+		},
+		{
+			EntryID: f.Entrys[1].ID,
+			Length:  "short",
+		},
+	}
+	var indexService = service.NewIndexService(
+		tx,
+		cookie.Store,
+		env,
+	)
+	t.Run("hairlength更新", func(t *testing.T) {
+		h := NewUpdateHandler(indexService)
+		body, err := json.Marshal(updateHeirLengths)
+		assert.NoError(t, err)
+		req := httptest.NewRequest(http.MethodPut, "/api/hairlength/update", bytes.NewBuffer(body))
+
+		w := httptest.NewRecorder()
+		h.ServeHTTP(w, req)
+
+		tx.RollbackCtx(ctx)
+		assert.Equal(t, http.StatusOK, w.Code)
+
+		var res []HairLength
+		err = json.Unmarshal(w.Body.Bytes(), &res)
+		assert.NoError(t, err)
+		assert.Equal(t, updateHeirLengths, res)
+	})
+}
