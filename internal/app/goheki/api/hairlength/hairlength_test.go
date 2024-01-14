e46e990a10c25e7de5ee34a9ed414a832e5b15d8
ygo <ka.ra.si.naporitan@gmail.com>
2024-01-14 11:10:55 +0900

    del

--- internal/app/goheki/api/hairlength/hairlength_test.go
+++ internal/app/goheki/api/hairlength/hairlength_test.go
@@ -277,3 +277,70 @@ func TestUpdateHairLengthHandler(t *testing.T) {
 		assert.Equal(t, updateHeirLengths, res)
 	})
 }
+
+func TestDeleteHairLengthHandler(t *testing.T) {
+	ctx := context.Background()
+	env, err := envconfig.NewEnv()
+	assert.NoError(t, err)
+	// データベースに接続
+	indexDB, cleanup, err := db.NewDBV1(ctx, "postgres", env.DatabaseURL)
+	assert.NoError(t, err)
+	defer cleanup()
+	// トランザクションの開始
+	tx, err := indexDB.BeginTxx(ctx, nil)
+	assert.NoError(t, err)
+
+	fixedTime := time.Date(2023, time.December, 27, 10, 55, 22, 0, time.UTC)
+	// データベースの準備
+	f := &fixtures.Fixture{DBv1: tx}
+	f.Build(t,
+		fixtures.NewSource(ctx, func(s *fixtures.Source) {
+			s.Name = "閃乱カグラ"
+			s.Url = "https://example.com/image1.png"
+			s.Type = "anime"
+		}).Connect(fixtures.NewEntry(ctx, func(s *fixtures.Entry) {
+			s.Name = "雪泉"
+			s.Image = "https://example.com/image1.png"
+			s.Content = "かわいい"
+			s.CreatedAt = fixedTime
+		}).Connect(fixtures.NewHairLength(ctx, func(s *fixtures.HairLength) {
+			s.Length = "short"
+		}))),
+		fixtures.NewSource(ctx, func(s *fixtures.Source) {
+			s.Name = "アイドルマスター"
+			s.Url = "https://example.com/image2.png"
+			s.Type = "game"
+		}).Connect(fixtures.NewEntry(ctx, func(s *fixtures.Entry) {
+			s.Name = "四条貴音"
+			s.Image = "https://example.com/image2.png"
+			s.Content = "お姫ちん"
+			s.CreatedAt = fixedTime
+		}).Connect(fixtures.NewHairLength(ctx, func(s *fixtures.HairLength) {
+			s.Length = "long"
+		}))),
+	)
+
+	// テストデータの準備
+	var indexService = service.NewIndexService(
+		tx,
+		cookie.Store,
+		env,
+	)
+	t.Run("hairlength削除", func(t *testing.T) {
+		h := NewDeleteHandler(indexService)
+		body, err := json.Marshal(IDs{IDs: []int64{*f.Entrys[0].ID, *f.Entrys[1].ID}})
+		assert.NoError(t, err)
+		req := httptest.NewRequest(http.MethodDelete, "/api/hairlength/delete", bytes.NewBuffer(body))
+
+		w := httptest.NewRecorder()
+		h.ServeHTTP(w, req)
+
+		tx.RollbackCtx(ctx)
+		assert.Equal(t, http.StatusOK, w.Code)
+
+		var res IDs
+		err = json.Unmarshal(w.Body.Bytes(), &res)
+		assert.NoError(t, err)
+		assert.Equal(t, []int64{*f.Entrys[0].ID, *f.Entrys[1].ID}, res.IDs)
+	})
+}
