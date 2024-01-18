706b82737fa983eb3d5cd5369c04dcc2d1759b52
ygo <ka.ra.si.naporitan@gmail.com>
2024-01-18 16:36:28 +0900

    update

--- internal/app/goheki/api/personality_type/personality_type.go
+++ internal/app/goheki/api/personality_type/personality_type.go
@@ -135,3 +135,45 @@ func (h *ReadHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
+
+type UpdateHandler struct {
+	svc *service.IndexService
+}
+
+func NewUpdateHandler(svc *service.IndexService) *UpdateHandler {
+	return &UpdateHandler{
+		svc: svc,
+	}
+}
+
+func (h *UpdateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPut {
+		return
+	}
+	var personalityTypes []PersonalityType
+	query := `
+		UPDATE
+			personality_type
+		SET
+			type = :type
+		WHERE
+			id = :id
+	`
+	err := json.NewDecoder(r.Body).Decode(&personalityTypes)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("json decode error: %v body:%v", err, r.Body))
+		return
+	}
+	for _, personalityType := range personalityTypes {
+		_, err = h.svc.DB.NamedExecContext(r.Context(), query, personalityType)
+		if err != nil {
+			log.Fatal(fmt.Sprintf("db error: %v", err))
+			return
+		}
+	}
+	err = json.NewEncoder(w).Encode(&personalityTypes)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("json encode error: %v", err))
+		return
+	}
+}
