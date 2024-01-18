8159926c07322f0af549b0d43e78c7c1a4254e77
ygo <ka.ra.si.naporitan@gmail.com>
2024-01-18 16:51:20 +0900

    update

--- internal/app/goheki/api/personality_type/personality_type_test.go
+++ internal/app/goheki/api/personality_type/personality_type_test.go
@@ -159,3 +159,65 @@ func TestReadPersonalityTypeHandler(t *testing.T) {
 	// ロールバック
 	tx.RollbackCtx(ctx)
 }
+
+func TestUpdatePersonalityTypeHandler(t *testing.T) {
+	ctx := context.Background()
+	env, err := envconfig.NewEnv()
+	assert.NoError(t, err)
+	// データベースに接続
+	indexDB, cleanup, err := db.NewDBV1(ctx, "postgres", env.DatabaseURL)
+	assert.NoError(t, err)
+	defer cleanup()
+	// トランザクションの開始
+	tx, err := indexDB.BeginTxx(ctx, nil)
+	assert.NoError(t, err)
+
+	// データベースの準備
+	f := &fixtures.Fixture{DBv1: tx}
+	f.Build(t,
+		fixtures.NewPersonalityType(ctx, func(s *fixtures.PersonalityType) {
+			s.Type = "大和撫子"
+		}),
+		fixtures.NewPersonalityType(ctx, func(s *fixtures.PersonalityType) {
+			s.Type = "天然"
+		}),
+	)
+
+	// テスト対象のハンドラを作成
+	var indexService = service.NewIndexService(
+		tx,
+		cookie.Store,
+		env,
+	)
+	t.Run("personality_type更新", func(t *testing.T) {
+		// リクエストの作成
+		updatePersonalityType := []PersonalityType{
+			{
+				ID:   f.PersonalityTypes[0].ID,
+				Type: "クール",
+			},
+			{
+				ID:   f.PersonalityTypes[1].ID,
+				Type: "ミステリアス",
+			},
+		}
+		b, err := json.Marshal(updatePersonalityType)
+		assert.NoError(t, err)
+		req := httptest.NewRequest(http.MethodPut,"/api/personality_type/update", bytes.NewBuffer(b))
+		// レスポンスの作成
+		w := httptest.NewRecorder()
+		// テスト対象のハンドラを実行
+		handler := NewUpdateHandler(indexService)
+		handler.ServeHTTP(w, req)
+		// レスポンスの検証
+		assert.Equal(t, http.StatusOK, w.Code)
+		// レスポンスのデコード
+		var res []PersonalityType
+		err = json.Unmarshal(w.Body.Bytes(), &res)
+		assert.NoError(t, err)
+		// レスポンスの検証
+		assert.Equal(t, updatePersonalityType, res)
+	})
+	// ロールバック
+	tx.RollbackCtx(ctx)
+}
