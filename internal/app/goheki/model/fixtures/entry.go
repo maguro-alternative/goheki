551cf0065e2bddf10fa4f6f214e20ea39f63b2c7
ygo <ka.ra.si.naporitan@gmail.com>
2024-01-18 10:33:57 +0900

    eyecolor追加

--- internal/app/goheki/model/fixtures/entry.go
+++ internal/app/goheki/model/fixtures/entry.go
@@ -64,6 +64,9 @@ func NewEntry(ctx context.Context, setter ...func(e *Entry)) *ModelConnector {
 			case *HekiRadarChart:
 				hekiRadarChart := connectingModel.(*HekiRadarChart)
 				hekiRadarChart.EntryID = entry.ID
+			case *EyeColor:
+				eyeColor := connectingModel.(*EyeColor)
+				eyeColor.EntryID = *entry.ID
 			default:
 				t.Fatalf("%T cannot be connected to %T", connectingModel, entry)
 			}
