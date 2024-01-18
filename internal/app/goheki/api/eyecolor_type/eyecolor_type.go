1edf9ae0c132b1f68332cef261fcd072ff60503e
ygo <ka.ra.si.naporitan@gmail.com>
2024-01-18 11:09:54 +0900

    read

---
+++ internal/app/goheki/api/eyecolor_type/eyecolor_type.go
@@ -0,0 +1,137 @@
+package eyecolortype
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/maguro-alternative/goheki/internal/app/goheki/service"
+	"github.com/maguro-alternative/goheki/pkg/db"
+
+	"encoding/json"
+	"net/http"
+)
+
+type CreateHandler struct {
+	svc *service.IndexService
+}
+
+func NewCreateHandler(svc *service.IndexService) *CreateHandler {
+	return &CreateHandler{
+		svc: svc,
+	}
+}
+
+func (h *CreateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		return
+	}
+	var eyeColorTypes []EyeColorType
+	query := `
+		INSERT INTO eyecolor_type (
+			color
+		) VALUES (
+			:color
+		)
+	`
+	err := json.NewDecoder(r.Body).Decode(&eyeColorTypes)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("json decode error: %v body:%v", err, r.Body))
+		return
+	}
+	for _, eyeColorType := range eyeColorTypes {
+		_, err = h.svc.DB.NamedExecContext(r.Context(), query, eyeColorType)
+		if err != nil {
+			log.Fatal(fmt.Sprintf("db error: %v", err))
+			return
+		}
+	}
+	err = json.NewEncoder(w).Encode(&eyeColorTypes)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("json encode error: %v", err))
+		return
+	}
+}
+
+type ReadHandler struct {
+	svc *service.IndexService
+}
+
+func NewReadHandler(svc *service.IndexService) *ReadHandler {
+	return &ReadHandler{
+		svc: svc,
+	}
+}
+
+func (h *ReadHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		return
+	}
+	var eyeColorTypes []EyeColorType
+	query := `
+		SELECT
+			id,
+			color
+		FROM
+			eyecolor_type
+		WHERE
+			id IN (?)
+	`
+	queryIDs, ok := r.URL.Query()["id"]
+	if !ok {
+		query = `
+			SELECT
+				id,
+				color
+			FROM
+				eyecolor_type
+		`
+		err := h.svc.DB.SelectContext(r.Context(), &eyeColorTypes, query)
+		if err != nil {
+			log.Fatal(fmt.Sprintf("db error: %v", err))
+			return
+		}
+		err = json.NewEncoder(w).Encode(&eyeColorTypes)
+		if err != nil {
+			log.Fatal(fmt.Sprintf("json encode error: %v", err))
+			return
+		}
+		return
+	} else if len(queryIDs) == 1 {
+		query = `
+			SELECT
+				id,
+				color
+			FROM
+				eyecolor_type
+			WHERE
+				id = ?
+		`
+		err := h.svc.DB.SelectContext(r.Context(), &eyeColorTypes, query, queryIDs[0])
+		if err != nil {
+			log.Fatal(fmt.Sprintf("db error: %v", err))
+			return
+		}
+		err = json.NewEncoder(w).Encode(&eyeColorTypes)
+		if err != nil {
+			log.Fatal(fmt.Sprintf("json encode error: %v", err))
+			return
+		}
+		return
+	}
+	query, args, err := db.In(query, queryIDs)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("db error: %v", err))
+		return
+	}
+	query = db.Rebind(len(queryIDs), query)
+	err = h.svc.DB.SelectContext(r.Context(), &eyeColorTypes, query, args...)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("db error: %v", err))
+		return
+	}
+	err = json.NewEncoder(w).Encode(&eyeColorTypes)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("json encode error: %v", err))
+		return
+	}
+}
