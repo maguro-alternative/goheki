99c54cbf0fbce92da3ceff7d24063c2d300e177e
ygo <ka.ra.si.naporitan@gmail.com>
2024-01-18 11:10:30 +0900

    update

--- internal/app/goheki/api/eyecolor_type/eyecolor_type.go
+++ internal/app/goheki/api/eyecolor_type/eyecolor_type.go
@@ -135,3 +135,45 @@ func (h *ReadHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
+
+type UpdateHandler struct {
+	svc *service.IndexService
+}
+
+func NewUpdateHandler(svc *service.IndexService) *UpdateHandler {
+	return &UpdateHandler{
+		svc: svc,
+	}
+}
+
+func (h *UpdateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPut {
+		return
+	}
+	var eyeColorTypes []EyeColorType
+	query := `
+		UPDATE
+			eyecolor_type
+		SET
+			color = :color
+		WHERE
+			id = :id
+	`
+	err := json.NewDecoder(r.Body).Decode(&eyeColorTypes)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("json decode error: %v body:%v", err, r.Body))
+		return
+	}
+	for _, eyeColorType := range eyeColorTypes {
+		_, err = h.svc.DB.NamedExecContext(r.Context(), query, eyeColorType)
+		if err != nil {
+			log.Fatal(fmt.Sprintf("db error: %v", err))
+			return
+		}
+	}
+	err = json.NewEncoder(w).Encode(&eyeColorTypes)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("json encode error: %v", err))
+		return
+	}
+}
