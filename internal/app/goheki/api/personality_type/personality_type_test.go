a466e4ac38cd9d2cf09d8503c009acd121b01395
ygo <ka.ra.si.naporitan@gmail.com>
2024-01-18 16:46:34 +0900

    read

---
+++ internal/app/goheki/api/personality_type/personality_type_test.go
@@ -0,0 +1,141 @@
+package personalitytype
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/maguro-alternative/goheki/configs/envconfig"
+	"github.com/maguro-alternative/goheki/internal/app/goheki/service"
+	"github.com/maguro-alternative/goheki/internal/app/goheki/service/cookie"
+
+	"github.com/maguro-alternative/goheki/pkg/db"
+
+	"github.com/maguro-alternative/goheki/internal/app/goheki/model/fixtures"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreatePersonalityTypeHandler(t *testing.T) {
+	ctx := context.Background()
+	env, err := envconfig.NewEnv()
+	assert.NoError(t, err)
+	// データベースに接続
+	indexDB, cleanup, err := db.NewDBV1(ctx, "postgres", env.DatabaseURL)
+	assert.NoError(t, err)
+	defer cleanup()
+	// トランザクションの開始
+	tx, err := indexDB.BeginTxx(ctx, nil)
+	assert.NoError(t, err)
+
+	// テスト対象のハンドラを作成
+	var indexService = service.NewIndexService(
+		tx,
+		cookie.Store,
+		env,
+	)
+	t.Run("personality_type登録", func(t *testing.T) {
+		// リクエストの作成
+		personalityType := []PersonalityType{
+			{
+				Type: "INTJ",
+			},
+			{
+				Type: "INTP",
+			},
+		}
+		b, err := json.Marshal(personalityType)
+		assert.NoError(t, err)
+		req := httptest.NewRequest(http.MethodPost, "/api/personality_type/create", bytes.NewBuffer(b))
+		// レスポンスの作成
+		w := httptest.NewRecorder()
+		// テスト対象のハンドラを実行
+		handler := NewCreateHandler(indexService)
+		handler.ServeHTTP(w, req)
+		// レスポンスの検証
+		assert.Equal(t, http.StatusOK, w.Code)
+		// レスポンスのデコード
+		var res []PersonalityType
+		err = json.Unmarshal(w.Body.Bytes(), &res)
+		assert.NoError(t, err)
+		// レスポンスの検証
+		assert.Equal(t, personalityType, res)
+	})
+	// ロールバック
+	tx.RollbackCtx(ctx)
+}
+
+func TestReadPersonalityTypeHandler(t *testing.T) {
+	ctx := context.Background()
+	env, err := envconfig.NewEnv()
+	assert.NoError(t, err)
+	// データベースに接続
+	indexDB, cleanup, err := db.NewDBV1(ctx, "postgres", env.DatabaseURL)
+	assert.NoError(t, err)
+	defer cleanup()
+	// トランザクションの開始
+	tx, err := indexDB.BeginTxx(ctx, nil)
+	assert.NoError(t, err)
+
+	// データベースの準備
+	f := &fixtures.Fixture{DBv1: tx}
+	f.Build(t,
+		fixtures.NewPersonalityType(ctx, func(s *fixtures.PersonalityType) {
+			s.Type = "大和撫子"
+		}),
+		fixtures.NewPersonalityType(ctx, func(s *fixtures.PersonalityType) {
+			s.Type = "天然"
+		}),
+	)
+
+	// テスト対象のハンドラを作成
+	var indexService = service.NewIndexService(
+		tx,
+		cookie.Store,
+		env,
+	)
+	t.Run("personality_type全件取得", func(t *testing.T) {
+		// リクエストの作成
+		req := httptest.NewRequest(http.MethodGet, "/api/personality_type/read", nil)
+		// レスポンスの作成
+		w := httptest.NewRecorder()
+		// テスト対象のハンドラを実行
+		handler := NewReadHandler(indexService)
+		handler.ServeHTTP(w, req)
+		// レスポンスの検証
+		assert.Equal(t, http.StatusOK, w.Code)
+		// レスポンスのデコード
+		var res []PersonalityType
+		err = json.Unmarshal(w.Body.Bytes(), &res)
+		assert.NoError(t, err)
+		// レスポンスの検証
+		assert.Equal(t, 2, len(res))
+		assert.Equal(t, "大和撫子", res[0].Type)
+		assert.Equal(t, "天然", res[1].Type)
+	})
+
+	t.Run("personality_type1件取得", func(t *testing.T) {
+		// リクエストの作成
+		req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/api/personality_type/read?id=%d", *f.PersonalityTypes[0].ID), nil)
+		// レスポンスの作成
+		w := httptest.NewRecorder()
+		// テスト対象のハンドラを実行
+		handler := NewReadHandler(indexService)
+		handler.ServeHTTP(w, req)
+		// レスポンスの検証
+		assert.Equal(t, http.StatusOK, w.Code)
+		// レスポンスのデコード
+		var res []PersonalityType
+		err = json.Unmarshal(w.Body.Bytes(), &res)
+		assert.NoError(t, err)
+		// レスポンスの検証
+		assert.Equal(t, 1, len(res))
+		assert.Equal(t, "大和撫子", res[0].Type)
+	})
+	// ロールバック
+	tx.RollbackCtx(ctx)
+}
