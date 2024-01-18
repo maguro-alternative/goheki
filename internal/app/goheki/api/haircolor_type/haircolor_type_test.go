1cba318762bda1570630a7303602e3c648c274c5
ygo <ka.ra.si.naporitan@gmail.com>
2024-01-18 11:46:04 +0900

    del

--- internal/app/goheki/api/haircolor_type/haircolor_type_test.go
+++ internal/app/goheki/api/haircolor_type/haircolor_type_test.go
@@ -208,3 +208,62 @@ func TestUpdateHairColorTypeHandler(t *testing.T) {
 	// ロールバック
 	tx.RollbackCtx(ctx)
 }
+
+func TestDeleteHairColorTypeHandler(t *testing.T) {
+	ctx := context.Background()
+	env, err := envconfig.NewEnv()
+	assert.NoError(t, err)
+	// データベースに接続
+	indexDB, cleanup, err := db.NewDBV1(ctx, "postgres", env.DatabaseURL)
+	assert.NoError(t, err)
+	defer cleanup()
+	// トランザクションの開始
+	tx, err := indexDB.BeginTxx(ctx, nil)
+	assert.NoError(t, err)
+
+	// データベースの準備
+	f := &fixtures.Fixture{DBv1: tx}
+	f.Build(t,
+		fixtures.NewHairColorType(ctx, func(h *fixtures.HairColorType) {
+			h.Color = "black"
+		}),
+		fixtures.NewHairColorType(ctx, func(h *fixtures.HairColorType) {
+			h.Color = "blue"
+		}),
+	)
+	// テスト対象のハンドラを作成
+	var indexService = service.NewIndexService(
+		tx,
+		cookie.Store,
+		env,
+	)
+	t.Run("haircolor_type削除", func(t *testing.T) {
+		// リクエストの作成
+		delIDs := IDs{
+			IDs: []int64{
+				*f.HairColorTypes[0].ID,
+				*f.HairColorTypes[1].ID,
+			},
+		}
+		b, err := json.Marshal(delIDs)
+		assert.NoError(t, err)
+		req := httptest.NewRequest(http.MethodDelete, "/api/haircolor_type/delete", bytes.NewBuffer(b))
+		// レスポンスの作成
+		w := httptest.NewRecorder()
+		// テスト対象のハンドラを実行
+		h := NewDeleteHandler(indexService)
+		h.ServeHTTP(w, req)
+
+		var res IDs
+		err = json.NewDecoder(w.Body).Decode(&res)
+		assert.NoError(t, err)
+		// レスポンスの検証
+		assert.Equal(t, http.StatusOK, w.Code)
+
+		assert.Equal(t, 2, len(res.IDs))
+		assert.Equal(t, *f.HairColorTypes[0].ID, res.IDs[0])
+		assert.Equal(t, *f.HairColorTypes[1].ID, res.IDs[1])
+	})
+	// ロールバック
+	tx.RollbackCtx(ctx)
+}
